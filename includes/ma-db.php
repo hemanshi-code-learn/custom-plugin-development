658b5920a12bfd0c0a70d51da8cb848e9497a7c1
Designer <designer@Mervans-MacBook-Pro.local>
2025-10-11 09:30:25 +0530

    Initial commit of the plugin

---
+++ includes/ma-db.php
@@ -0,0 +1,89 @@
+<?php 
+
+class Contact_Form_DB{
+
+    private static $instance = null;
+    private $table_name;
+
+    /**
+     * Singleton Pattern: Ensures only one instance of the class exists.
+     */
+
+     public static function get_instance(){
+        if(is_null(self::$instance)){
+            self::$instance = new self();
+        }
+        return self::$instance;
+     }
+
+     private function __construct(){
+        global $wpdb;
+        $this->table_name = $wpdb->prefix . 'ma_cf_submissions';
+     }
+    
+
+     /**
+      * Creates the database table on plugin activation.
+      */
+
+      public function create_table(){
+        global $wpdb;
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $sql = "CREATE TABLE $this->table_name(
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                firstname tinytext NOT NULL,
+                lastname tinytext NOT NULL,
+                email varchar(100) NOT NULL,
+                phone varchar(20) DEFAULT '' NOT NULL,
+                message text NOT NULL,
+                PRIMARY KEY (id) 
+                ) $charset_collate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql); // Safe way to create/update tables
+      }
+
+     /**
+      * Deletes the database table.
+      */
+
+      public function delete_table(){
+        global $wpdb;
+        $wpdb->query("DROP TABLE IF EXISTS $this->table_name");
+      }
+
+
+     /**
+      * Inserts sanitized form data into the database.
+      */
+
+      public function insert_data($data){
+        global $wpdb;
+        return $wpdb->insert(
+            $this->table_name,
+            [
+                'firstname' => $data['cf_firstname'],
+                'lastname' => $data['cf_lastname'],
+                'email' => $data['cf_email'],
+                'phone' => $data['cf_phone'],
+                'message' => $data['cf_message'],
+            ],
+            //Format Placeholders
+            [ '%s', '%s', '%s', '%s', '%s' ]
+            );
+      }
+
+
+     /**
+      * Retrieves all submission data for the admin panel.
+      */
+
+      public function get_all_submissions(){
+        global $wpdb;
+        return $wpdb->get_results("SELECT * FROM $this->table_name ORDER BY time DESC", ARRAY_A);
+      }
+
+}
+?>
---
+++ ma-contact-form.php
@@ -0,0 +1,53 @@
+<?php 
+/*
+Plugin Name: MA Contact Form
+Description: A contact form plugin.
+Version: 1.0
+Author: Mervan Agency
+Author URI:
+*/
+
+if(!defined('ABSPATH')){
+    exit;
+}
+
+
+define('CONTACT_FORM_PATH', plugin_dir_path(__FILE__));
+
+// echo '<pre>';
+// var_dump(CONTACT_FORM_PATH);
+// echo '</pre>';
+
+require_once CONTACT_FORM_PATH . 'includes/ma-admin.php';
+require_once CONTACT_FORM_PATH . 'includes/ma-db.php';
+require_once CONTACT_FORM_PATH . 'includes/ma-core.php';
+// echo '<pre>';
+// var_dump(CONTACT_FORM_PATH . 'includes/ma-admin.php');
+// echo '</pre>';
+
+
+/**
+ * Runs on plugin activation(Creates the database table)
+ */
+
+function macf_activate_plugin(){
+    Contact_Form_DB::get_instance()->create_table();
+}
+register_activation_hook(__FILE__, 'macf_activate_plugin');
+
+function macf_deactivate_plugin(){
+    $delete_data = get_option('macf_delete_data_on_deactivation', 'no');
+    if($delete_data === 'yes'){
+        Contact_Form_DB::get_instance()->delete_table();
+    }
+}
+register_deactivation_hook(__FILE__, 'macf_deactivate_plugin')
+
+
+
+/**
+ * Initialize the core plugin class instance.
+ */
+// OOP_Contact_Form::get_instance();
+
+?>
