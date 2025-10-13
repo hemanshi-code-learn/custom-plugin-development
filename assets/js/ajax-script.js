f11175311a127529eb61b24429179aac67f759c8
Designer <designer@Mervans-MacBook-Pro.local>
2025-10-13 13:51:17 +0530

    Add ajax handling and css styling.

---
+++ assets/js/ajax-script.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($){
+    $('#ma-contact-form').on('submit', function(e){
+        e.preventDefault();
+
+        var formData = ($this).serializeArray();
+
+        formData.push({
+            name:'action',
+            value:'ma_submit_form'
+        });
+
+        var $responseDiv = $('#ma-form-response');
+        $responseDiv.html('Sending...').removeClass('error success').addClass('sending');
+
+        $.ajax({
+            type: 'POST',
+            url: maContactFormAjax.ajaxurl,
+            data:formData,
+            success: function(response){
+                if(response.success){
+                    $responseDiv.html(response.data.message).removeClass('sending error').addClass('success');
+                    $('#ma-contact-form')[0].reset();
+                } else {
+                    $responseDiv.html(response.data.message).removeClass(sending success').addClass('error');
+                }
+            },
+            error: function({
+                $responseDiv.html('An unexpected error occurred.').removeClass('sending success').addClass('error');
+            })
+        });
+    });
+});
